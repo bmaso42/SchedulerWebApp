9ba7fbec161ecb44e42e7baa8fdb2de650c4725c
Brian Mason <bmaso42@wgu.edu>
2022-01-26 16:59:18 -0800

    added Main controller, building Models

--- SchedulerWebApp/Controllers/HomeController.cs
+++ SchedulerWebApp/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchedulerWebApp.Controllers
     public class HomeController : Controller
     {
         MySqlConnection con = new MySqlConnection();
-        MySqlCommand cmd = new MySqlCommand();
+        //MySqlCommand cmd = new MySqlCommand();
         MySqlDataReader dr;
 
         [HttpGet]
@@ -36,22 +36,31 @@ namespace SchedulerWebApp.Controllers
 
         void connectionString()
         {
-            con.ConnectionString = "server=localhost;uid=sqlUser;pwd=Passw0rd!;database=client_schedule";
+            con.ConnectionString = "server=localhost;user=sqlUser;database=client_schedule;port=3306;password=Passw0rd!;";
 
         }
         [HttpPost]
         public ActionResult Verify(Login login)
         {
+            //connectionString();
+            //con.Open();
+            //cmd.Connection = con;
+            //cmd.CommandText = "select * from user where userName='"+login.Name+"' and password='"+login.Password+"'";
+            //dr = cmd.ExecuteReader();
             connectionString();
             con.Open();
-            cmd.Connection = con;
-            cmd.CommandText = "select * from user where userName='"+login.Name+"' and password='"+login.Password+"'";
+
+            string sql = "select * from client_schedule.user where userName=@Name and password=@Password";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Name", login.Name);
+            cmd.Parameters.AddWithValue("@Password", login.Password);
             dr = cmd.ExecuteReader();
+
             if (dr.Read())
             {
 
                 con.Close();
-                return View("SuccessView");
+                return RedirectToAction("Index", "Main");
             }
             else
             {
---
+++ SchedulerWebApp/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchedulerWebApp.Models
+{
+    public class Customer
+    {
+    }
+}
